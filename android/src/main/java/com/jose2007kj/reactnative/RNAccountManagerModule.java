64fb0256caab171228ceef8d0b8050efeac84050
jose2007kj <jose2007kj@gmail.com>
2018-11-05 23:00:30 +0530

    added native methods

--- android/src/main/java/com/jose2007kj/reactnative/RNAccountManagerModule.java
+++ android/src/main/java/com/jose2007kj/reactnative/RNAccountManagerModule.java
@@ -5,6 +5,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 
 public class RNAccountManagerModule extends ReactContextBaseJavaModule {
 
@@ -19,4 +21,24 @@ public class RNAccountManagerModule extends ReactContextBaseJavaModule {
   public String getName() {
     return "RNAccountManager";
   }
+  @ReactMethod
+	public void getAccounts (final Promise promise) {
+		AccountManager am = AccountManager.get(this.reactContext);
+		Account [] acc = am.getAccounts();
+		if (acc.length > 0){
+			String s = "";
+			for (int i=0; i<acc.length; i++){
+				s += acc[i] + "\n";
+			}
+			
+
+		try {
+                            map.putString("account", s);
+                            
+                            promise.resolve(map);
+                        } catch (Exception e) {
+                            map.putString("account", "COULD_NOT_FETCH");
+                            promise.reject("COULD_NOT_FETCH", map.toString());
+                        }
+	}
 }
