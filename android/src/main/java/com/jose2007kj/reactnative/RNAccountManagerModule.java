e5f8d124a9dd4b197ba8bd9d94cbd9219a284897
jose2007kj <jose2007kj@gmail.com>
2019-08-27 12:01:24 +0530

    added functions to fetch current timestamp and recently updated contacts

--- android/src/main/java/com/jose2007kj/reactnative/RNAccountManagerModule.java
+++ android/src/main/java/com/jose2007kj/reactnative/RNAccountManagerModule.java
@@ -6,9 +6,13 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import android.accounts.Account;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
+import java.util.Iterator;
 import android.accounts.AccountManager;
 import android.content.ContentResolver;
 import android.database.Cursor;
@@ -32,7 +36,16 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import com.facebook.react.bridge.ActivityEventListener;
 import android.provider.Telephony;
-
+import java.security.MessageDigest;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import java.security.NoSuchAlgorithmException;
+import android.content.ActivityNotFoundException;
+import java.util.concurrent.TimeUnit;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 public class RNAccountManagerModule extends ReactContextBaseJavaModule {
   private final ReactApplicationContext reactContext;
 
@@ -52,29 +65,29 @@ public class RNAccountManagerModule extends ReactContextBaseJavaModule {
   //      if (requestCode == 103) {
   //        Log.d("paytm", "staret activity success: "+resultCode);
   //          // Make sure the request was successful
-           
+
   //      }
   //  }
   @ReactMethod
-	public void getAccounts(final Promise promise) {
-		AccountManager am = AccountManager.get(this.reactContext);
-	  Account [] acc = am.getAccountsByType("com.google");//am.getAccounts();//
-		if (acc.length > 0){
-			String s = "";
-			for (int i=0; i<acc.length; i++){
-				s += acc[i] + "\n";
-			}
-			final WritableMap map = Arguments.createMap();
+  public void getAccounts(final Promise promise) {
+    AccountManager am = AccountManager.get(this.reactContext);
+    Account [] acc = am.getAccountsByType("com.google");//am.getAccounts();//
+    if (acc.length > 0){
+      String s = "";
+      for (int i=0; i<acc.length; i++){
+        s += acc[i] + "\n";
+      }
+      final WritableMap map = Arguments.createMap();
 
-		try {
+    try {
                             map.putString("account", s);
-                            
+
                             promise.resolve(map);
                         } catch (Exception e) {
                             map.putString("account", "COULD_NOT_FETCH");
                             promise.reject("COULD_NOT_FETCH", map.toString());
                         }
-	}
+  }
   }
   @ReactMethod
   public void sendSms(String mobileNo,String message){
@@ -95,14 +108,14 @@ public class RNAccountManagerModule extends ReactContextBaseJavaModule {
       sendIntent.putExtra("exit_on_sent", true);
       sendIntent.putExtra("address", mobileNo);
       reactContext.startActivity(sendIntent);
-      
+
     }catch(Exception e){
       Log.d("sms ", "sms error: "+e.toString());
     }
   }
   @ReactMethod
-	public void openPaytmApp(String mobileNo,String amount) {
-	  final WritableMap map = Arguments.createMap();
+  public void openPaytmApp(String mobileNo,String amount) {
+    final WritableMap map = Arguments.createMap();
 
     try {
       Bundle bundle  = new Bundle();
@@ -111,48 +124,127 @@ public class RNAccountManagerModule extends ReactContextBaseJavaModule {
       bundle.putBoolean("isMobile Number Editable", false);
       bundle.putBoolean("is Amount Editable", true);
       Intent startPaytm = new Intent();
-      startPaytm.setComponent(new ComponentName("net.one97.paytm", "net.one97.paytm.AJRJarvisSplash")); 
+      startPaytm.setComponent(new ComponentName("net.one97.paytm", "net.one97.paytm.AJRJarvisSplash"));
       startPaytm.putExtra("paymentmode", 1);
       startPaytm.putExtra("bill", bundle);
       getCurrentActivity().startActivity(startPaytm);
       Log.d("paytm", "staretd activity: ");
       //todo:maybe in later stage we can handle on activiyresult
       // map.putString("account", s);
-        
+
         // promise.resolve(map);
     } catch (Exception e) {
       Log.d("paytm", "staret activity exception: "+e);
         // map.putString("account", "COULD_NOT_FETCH");
         // promise.reject("COULD_NOT_FETCH", map.toString());
     }
-	
+
   }
- 
+
 
 
   @ReactMethod
-	public void checkAccounts(final Promise promise) {
-		AccountManager am = AccountManager.get(this.reactContext);
-	  Account [] acc = am.getAccounts();//am.getAccountsByType("com.google");
-		if (acc.length > 0){
-			String s = "";
-			for (int i=0; i<acc.length; i++){
-				s += acc[i] + "\n";
-			}
-			final WritableMap map = Arguments.createMap();
+  public void checkAccounts(final Promise promise) {
+    AccountManager am = AccountManager.get(this.reactContext);
+    Account [] acc = am.getAccounts();//am.getAccountsByType("com.google");
+    if (acc.length > 0){
+      String s = "";
+      for (int i=0; i<acc.length; i++){
+        s += acc[i] + "\n";
+      }
+      final WritableMap map = Arguments.createMap();
 
-		try {
+    try {
                             map.putString("account", s);
-                            
+
                             promise.resolve(map);
                         } catch (Exception e) {
                             map.putString("account", "COULD_NOT_FETCH");
                             promise.reject("COULD_NOT_FETCH", map.toString());
                         }
-	}
   }
+  }
+  @ReactMethod void getTimeStamp(final Promise promise){
 
-  
+        final WritableMap map = Arguments.createMap();
+    try{
+        String ts = String.valueOf(System.currentTimeMillis());
+        map.putString("timestamp",ts);
+        promise.resolve(map);
+      }catch(Exception e){
+         map.putString("timestamp",e.toString());
+         promise.reject("failed",map.toString());
+      }
+  }
+  @ReactMethod
+  public void getRecentlyUpdatedConacts(String lastUpdate,final Promise promise){
+
+      final WritableMap map = Arguments.createMap();
+      try{
+      ContentResolver contentResolver = this.reactContext.getContentResolver();
+      Uri uri = ContactsContract.Contacts.CONTENT_URI;
+      String ts = String.valueOf(System.currentTimeMillis());
+      Cursor cursor = contentResolver.query(uri,
+        null,
+        ContactsContract.Contacts.CONTACT_LAST_UPDATED_TIMESTAMP + ">=?",
+        new String[]{lastUpdate},
+        null        // Ordering
+        );
+      WritableMap contactmap = Arguments.createMap();
+      JSONObject updatedContact= new JSONObject();
+      JSONArray contactList = new JSONArray();
+      if (cursor.getCount() > 0) {
+        JSONArray numberList;
+        JSONObject numberObj;
+        JSONArray emailList;
+        JSONObject emailObj;
+        JSONObject contactObj;
+        String pno;
+        String eml;
+        int i;
+        while(cursor.moveToNext()){
+            contactObj = new JSONObject();
+            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            contactObj.put("id",contactId);
+            contactObj.put("name",name);
+            Cursor phones = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+            Cursor emails = contentResolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + contactId, null, null);
+            numberList= new JSONArray();
+            i=0;
+            while (phones.moveToNext()) {
+                 numberObj= new JSONObject();
+                 pno=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                 numberObj.put("id",i);
+                 numberObj.put("number",pno);
+                 numberList.put(numberObj);
+                 i++;
+            }
+            i=0;
+            emailList= new JSONArray();
+            while (emails.moveToNext()) {
+                    eml=emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                    emailObj= new JSONObject();
+                    emailObj.put("id",i);
+                    emailObj.put("email",eml);
+                    emailList.put(emailObj);
+                    i++;
+            }
+            contactObj.put("numbers",numberList);
+            contactObj.put("emails",emailList);
+            contactList.put(contactObj);
+        }
+      }
+      updatedContact.put("updatedContacts",contactList);
+      contactmap= convertJsonToMap(updatedContact);
+      Log.d("contact sync", "recently updated function contacts time stamp: "+ts);
+      contactmap.putString("timestamp",ts);
+      promise.resolve(contactmap);
+      }catch(Exception e){
+          map.putString("timestamp",e.toString());
+          promise.reject("failed",map.toString());
+      }
+  }
   @ReactMethod
   public void identifyWhatsappContact(final Promise promise) {
         ContentResolver contentResolver = this.reactContext.getContentResolver();
@@ -185,13 +277,30 @@ public class RNAccountManagerModule extends ReactContextBaseJavaModule {
         Cursor cursor1 = this.reactContext.getContentResolver().query(ContactsContract.RawContacts.CONTENT_URI, projection1, selection, selectionArgs, null);
         boolean hasWhatsApp = cursor1.moveToNext();
         final WritableMap map = Arguments.createMap();
-        
+
         if (hasWhatsApp){
             map.putString("contact","true");
             promise.resolve(map);
         }else{
         map.putString("contact","false");
         promise.resolve(map);}
+}
+//function for openining instgram app for authentication instead of web browser
+@ReactMethod
+public  void openInstagram(String url) {
+  Log.d("instagram deeplink:", "insta 007"+url);
+  Uri uri = Uri.parse(url);
+  Intent insta = new Intent(Intent.ACTION_VIEW, uri);
+  insta.setPackage("com.instagram.android");
+
+  try {
+      getCurrentActivity().startActivity(insta);
+      Log.d("instagram deeplink inside:", "insta 008");
+  } catch (ActivityNotFoundException e) {
+      Log.d("KeyHash:", "insta 009"+e);
+      getCurrentActivity().startActivity(new Intent(Intent.ACTION_VIEW,
+      Uri.parse(url)));
+  }
 }
 // function for converting url to base64
 
@@ -217,17 +326,66 @@ public void urlTobase64(String url, Promise promise) {
                   map.putString("base64", "COULD_NOT_FETCH");
                   promise.reject("COULD_NOT_FETCH", map.toString());
               }
-      
-    
+
+
 } catch (Exception e) {
     final WritableMap map = Arguments.createMap();
     map.putString("error", "COULD_NOT_FETCH");
                   promise.reject("COULD_NOT_FETCH", map.toString());
     // Log.d("Error", e.toString());
 }
-    
+
 
 }
 //base64 function ends here
+   private static WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
+    WritableMap map = new WritableNativeMap();
+
+    Iterator<String> iterator = jsonObject.keys();
+    while (iterator.hasNext()) {
+        String key = iterator.next();
+        Object value = jsonObject.get(key);
+        if (value instanceof JSONObject) {
+            map.putMap(key, convertJsonToMap((JSONObject) value));
+        } else if (value instanceof  JSONArray) {
+            map.putArray(key, convertJsonToArray((JSONArray) value));
+        } else if (value instanceof  Boolean) {
+            map.putBoolean(key, (Boolean) value);
+        } else if (value instanceof  Integer) {
+            map.putInt(key, (Integer) value);
+        } else if (value instanceof  Double) {
+            map.putDouble(key, (Double) value);
+        } else if (value instanceof String)  {
+            map.putString(key, (String) value);
+        } else {
+            map.putString(key, value.toString());
+        }
+    }
+    return map;
+}
+
+private static WritableArray convertJsonToArray(JSONArray jsonArray) throws JSONException {
+    WritableArray array = new WritableNativeArray();
+
+    for (int i = 0; i < jsonArray.length(); i++) {
+        Object value = jsonArray.get(i);
+        if (value instanceof JSONObject) {
+            array.pushMap(convertJsonToMap((JSONObject) value));
+        } else if (value instanceof  JSONArray) {
+            array.pushArray(convertJsonToArray((JSONArray) value));
+        } else if (value instanceof  Boolean) {
+            array.pushBoolean((Boolean) value);
+        } else if (value instanceof  Integer) {
+            array.pushInt((Integer) value);
+        } else if (value instanceof  Double) {
+            array.pushDouble((Double) value);
+        } else if (value instanceof String)  {
+            array.pushString((String) value);
+        } else {
+            array.pushString(value.toString());
+        }
+    }
+    return array;
+}
 
 }
